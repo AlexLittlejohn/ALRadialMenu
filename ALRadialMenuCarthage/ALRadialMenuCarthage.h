a3f17e5e304a7564275f2ff2000cba9993b1c0fa
Dan <dan.burkhardt@ibm.com@Dans-MacBook-Pro.local>
2017-08-16 11:50:42 -0400

    Support for carthage integrated into application from original developer source

---
+++ ALRadialMenuCarthage/ALRadialMenuCarthage.h
@@ -0,0 +1,19 @@
+//
+//  ALRadialMenuCarthage.h
+//  ALRadialMenuCarthage
+//
+//  Created by Dan on 8/16/17.
+//  Copyright © 2017 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALRadialMenuCarthage.
+FOUNDATION_EXPORT double ALRadialMenuCarthageVersionNumber;
+
+//! Project version string for ALRadialMenuCarthage.
+FOUNDATION_EXPORT const unsigned char ALRadialMenuCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALRadialMenuCarthage/PublicHeader.h>
+
+
